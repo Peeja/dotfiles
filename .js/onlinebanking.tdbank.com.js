6f1ef1e892298fddef7d779f7e80d1e49be92818
Peter Jaros <peter.a.jaros@gmail.com>
2014-03-07 18:16:20 -0500

    Good work, TD Bank.

--- .js/onlinebanking.tdbank.com.js
+++
@@ -1,5 +0,0 @@
-// Turn the user field into a text field, instead of a password field, so that
-// LassPass can fill it in.
-if (window.name == "main") {
-  $("input[name='user']").get(0).type = "text";
-}
