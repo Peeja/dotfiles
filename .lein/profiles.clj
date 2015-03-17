8c95b67a10dc7cd43e25f57b506f5b86cc1d72b6
Peter Jaros <peter.a.jaros@gmail.com>
2015-03-17 18:41:50 -0400

    .lein/profiles.clj

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,1 +1,6 @@
-{:user {:plugins [[cljs-kickoff/lein-template "0.1.5"]]}}
+{:user {:dependencies [[com.cemerick/pomegranate "0.3.0"]
+                       [slamhound "1.5.5"]]
+        :plugins [[cider/cider-nrepl "0.7.0"]
+                  [lein-cprint "1.0.0"]
+                  [lein-localrepo "0.5.2"]]
+        :aliases {"slamhound" ["run" "-m" "slam.hound"]}}}
